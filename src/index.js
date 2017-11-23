41faaf241b1e296bd7c0c0b60d1686c8c24839e3
Olexandr Matkovsky <savio.km.ua@gmail.com>
2017-11-23 18:01:05 +0200

    added admin panel

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-import './main.css'
-
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
+import {Router, Route, browserHistory} from 'react-router'
+import {BrowserRouter} from 'react-router-dom'
+import {syncHistoryWithStore} from 'react-router-redux'
 import {createStore, applyMiddleware} from 'redux'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import {syncHistoryWithStore} from 'react-router-redux'
-import {Router, Route, browserHistory} from 'react-router'
 import {Provider} from 'react-redux'
-
-import reducers from 'reducers'
+import reducer from 'reducers'
+import Layout from 'containers/layout'
+import AdminLayout from 'containers/admin'
+import {fetchPosts} from 'actions/posts'
 import Posts from 'containers/posts'
+import AdminPosts from 'containers/admin/posts'
 
-const store = createStore(reducers, composeWithDevTools(
-  applyMiddleware(thunk)
-))
-const history = syncHistoryWithStore(browserHistory, store)
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Route path='/' component={Posts}/>
-    </Router>
-  </Provider>,
-  document.getElementById('root')
-);
+(function () {
+  const history = syncHistoryWithStore(browserHistory, store)
+  const promises = [store.dispatch(fetchPosts())]
+  Promise.all(promises).then(() => {
+    render(
+      (<Provider store={store}>
+        <BrowserRouter>
+          <Router history={history}>
+            <Route component={Layout}>
+              <Route path='/' component={Posts}/>
+            </Route>
+            <Route path='/admin' component={AdminLayout}>
+              <Route path='/admin/posts' component={AdminPosts}/>
+            </Route>
+
+          </Router>
+        </BrowserRouter>
+      </Provider>
+      ), document.getElementById('root'))})
+})()
